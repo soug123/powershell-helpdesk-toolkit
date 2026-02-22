9c84571782b7b6c95949a56de6c2af8769f9fb59
jorge amaya <amaya.jr@hotmail.com>
2026-02-22 16:35:21 -0500

    Add script to create directory and export services
    This script creates a new directory, a report file, lists the files in the directory, and exports running services to a text file.

--- service-report.ps1
+++ service-report.ps1
@@ -1,1 +1,11 @@
+# Create a new folder
+New-Item -Path "C:\Temp\ITLab" -ItemType Directory
 
+# Create a file
+New-Item -Path "C:\Temp\ITLab\report.txt" -ItemType File
+
+# List files in directory
+Get-ChildItem "C:\Temp\ITLab"
+
+# Export running services to a text file
+Get-Service | Out-File "C:\Temp\ITLab\services.txt"

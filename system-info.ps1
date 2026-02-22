736abc3f1ef1a38026008b2f08b9cb8175a33e4d
jorge amaya <amaya.jr@hotmail.com>
2026-02-22 16:34:13 -0500

    Add system information retrieval commands

--- system-info.ps1
+++ system-info.ps1
@@ -1,1 +1,21 @@
+# Get basic system information
+Get-ComputerInfo
 
+# Get operating system info
+Get-CimInstance Win32_OperatingSystem
+
+# Check current user
+whoami
+
+# List running processes
+Get-Process | Sort-Object CPU -Descending | Select-Object -First 10# Get basic system information
+Get-ComputerInfo
+
+# Get operating system info
+Get-CimInstance Win32_OperatingSystem
+
+# Check current user
+whoami
+
+# List running processes
+Get-Process | Sort-Object CPU -Descending | Select-Object -First 10

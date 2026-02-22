85bce23bc44e0bb4489892f0813cd2a2e5427b61
jorge amaya <amaya.jr@hotmail.com>
2026-02-22 17:42:43 -0500

    Delete system-info.ps1

--- system-info.ps1
+++
@@ -1,9 +0,0 @@
-# System Information Script
-
-Write-Host "=== System Information ===" -ForegroundColor Cyan
-
-Get-ComputerInfo | Select-Object WindowsProductName, WindowsVersion
-whoami
-
-Write-Host "`nTop 5 CPU Processes:" -ForegroundColor Yellow
-Get-Process | Sort-Object CPU -Descending | Select-Object -First 5
